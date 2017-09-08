fce2672aaf51bee37f942128fb68e8d18b80e1a0
Albin Fyhr <albinfyhr@gmail.com>
2017-09-08 08:33:40 +0200

    created game
    created game

---
+++ clicker game/Assets/scripts/click.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class click : MonoBehaviour {
+
+	public int countNumber = 0;
+	public GameObject countText;
+	public GameObject firstText;
+	public GameObject counter;
+
+	public void clickFunc() {
+		countNumber += 1;
+	}
+
+	void Update () {
+		if (countNumber >= 1) {
+            countText.GetComponent<Text>().text = countNumber.ToString();
+            firstText.SetActive(false);
+            counter.SetActive(true);
+        }
+	}
+}
---
+++ clicker game/Assets/scripts/upgrades.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upgrades : MonoBehaviour {
+
+    public GameObject scriptObject;
+    public GameObject autoClickUpg1;
+
+    public int autoClickUpg1Done = 0;
+    private int clicked;
+    private click otherScript;
+
+	void Start () {
+        otherScript = scriptObject.GetComponent<click>();
+        clicked = otherScript.countNumber;
+	}
+
+    void Update()
+    {
+        if(autoClickUpg1Done == 1)
+        {
+            StartCoroutine(Autoclick());
+        }
+    }
+
+    public void Autoclick1 () {
+            autoClickUpg1Done += 1;
+	}
+
+    IEnumerator Autoclick()
+    {
+        yield return new WaitForSeconds(1);
+        clicked += 1;
+    }
+}
